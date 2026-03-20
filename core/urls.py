34fda060859e13c17434ceb510cd0bcac469e21b
Samuel <samueloritu2019@gmail.com>
2026-03-20 20:56:42 +0100

    added new pages to views and updated the url.py

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name = 'contact'),
+    path('experience/', views.experience, name='experience'),
+    path('blog/', views.blog, name='blog'),
 ]
+
--- core/views.py
+++ core/views.py
@@ -5,3 +5,21 @@ from django.http import HttpResponse, JsonResponse
 def index(request):
     context = "Welcome to my First Django project, This is the index page"
     return HttpResponse(context)
+
+def about(request):
+    myself = {"Name": "Samuel Oreofe Oritu"}
+    return JsonResponse(myself)
+
+def contact(request):
+    details = {"Phone Number": "2347049292634"}
+    return JsonResponse(details)
+
+def experience(request):
+    me = """ 
+SQI College of ICT
+"""
+    return HttpResponse(me)
+
+def blog(request):
+    blog = "Empty"
+    return HttpResponse(blog)
