c15f6ff5403a7582359055e8c0e1882649cd8a01
Samuel <samueloritu2019@gmail.com>
2026-03-20 21:30:05 +0100

    Added a template folder, html, also updated views

--- core/views.py
+++ core/views.py
@@ -1,25 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+# from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    context = "Welcome to my First Django project, This is the index page"
-    return HttpResponse(context)
+    # context = "Welcome to my First Django project, This is the index page"
+    # return HttpResponse(context)
+    return render(request, 'core/index.html')
 
 def about(request):
-    myself = {"Name": "Samuel Oreofe Oritu"}
-    return JsonResponse(myself)
+    # myself = {"Name": "Samuel Oreofe Oritu"}
+    # return JsonResponse(myself)
+    return render(request, 'core/about.html')
 
 def contact(request):
-    details = {"Phone Number": "2347049292634"}
-    return JsonResponse(details)
+    # details = {"Phone Number": "2347049292634"}
+    # return JsonResponse(details)
+    return render(request, 'core/contact.html')
 
 def experience(request):
-    me = """ 
-SQI College of ICT
-"""
-    return HttpResponse(me)
+#     me = """ 
+# SQI College of ICT
+# """
+    return render(request, 'core/experience.html')
 
 def blog(request):
-    blog = "Empty"
-    return HttpResponse(blog)
+    # blog = "Empty"
+    # return HttpResponse(blog)
+    return render(request, 'core/blog.html')
