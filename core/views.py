744180ac1cf7f35dd377cdb695b9cce3464aaaaa
Samuel <samueloritu2019@gmail.com>
2026-03-20 20:15:28 +0100

    Modified setting by adding Core in the installed app, modified url and views too

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
+
+def index(request):
+    context = "Welcome to my First Django project, This is the index page"
+    return HttpResponse(context)
